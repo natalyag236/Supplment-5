f11e7e753b3bff187e79e69cb00da562d5645bf6
Natalya Goldsmith <ngol1755@students.vsu.edu>
2024-11-25 23:20:18 -0500

    feat:implement sqaure root calcuation function

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import math
+def calculate_sqaure_root(number):
+    if number < 0:
+        raise ValueError("Cannot calculte the sqaure root of a negative number")
+    return math.sqrt(number)
 
 def test_calculate_sqaure_root():
     assert calculate_sqaure_root(9) == 3
